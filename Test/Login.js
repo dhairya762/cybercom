b73b29abd38cb24c66d2821e33d39882f6d45e8a
DHAIRYA PATEL <65170659+dhairya762@users.noreply.github.com>
2021-02-06 13:07:13 +0530

    Commit 1

---
+++ Test/Login.js
@@ -0,0 +1,13 @@
+function validateForm()
+{
+    var obj1 = document.["form"]["username"].value;
+    var obj2= document.["form"]["password"].value;
+    if (x == "") 
+    {
+    
+        alert("Name must be filled out");
+        return false;
+    }
+    console.log(obj1);
+    console.log(obj2);
+}
